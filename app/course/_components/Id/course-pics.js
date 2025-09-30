ae251ed730f9a70bb5e125e0f391ea6ba7a87616
Wang-Hsiang <guitarwang0507@gmail.com>
2025-09-30 17:49:23 +0800

    img添加css

--- app/course/_components/Id/course-pics.js
+++ app/course/_components/Id/course-pics.js
@@ -11,7 +11,7 @@ export default function CoursePics({ imgs }) {
       <div className={styles.left}>
         <div className={styles.pics}>
           {imgs?.slice(1).map((img) => (
-            <img key={img.id} src={`/course/img/${img?.url}`} alt="" />
+            <img className={styles.img} key={img.id} src={`/course/img/${img?.url}`} alt="" />
           ))}
         </div>
       </div>
